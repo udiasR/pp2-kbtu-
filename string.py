7b6ddbc77604557001e9d6a1ace61068f4bbd661
Your Name <ulykbekdias6@gmail.com>
2025-01-24 23:21:50 +0500

    lab1

---
+++ string.py
@@ -0,0 +1,38 @@
+print("Hello")
+print('Hello')
+
+
+
+#Slicing
+
+b = "Hello, World!"
+print(b[2:5])
+
+
+#Modify Strings
+
+a = "Hello, World!"
+print(a.upper())
+
+
+#String Concatenation
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#String Format
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#escape character
+txt = "We are the so-called \"Vikings\" from the north."
+
+#Methods
+# capitalize()	Converts the first character to upper case
+
+a = "Hello, World!"
+print(a.capitalize())
+
---
+++ variables.py
@@ -0,0 +1,39 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+# Variable Names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+# Many Values to Multiple Variables
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+# Output Variables
+
+x = "Python is awesome"
+print(x)
+
+
+# Global Variables
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
