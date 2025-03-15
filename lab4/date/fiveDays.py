62e7df2931fb78c0f5d78885317ad2c14e3bb564
udias <ulykbekdias6@gmail.com>
2025-03-15 14:57:22 +0500

    Update fiveDays.py

--- lab4/date/fiveDays.py
+++ lab4/date/fiveDays.py
@@ -1,8 +1,10 @@
 
 
 import datetime
-#Yo Dana wassup my nigga
+
 time_now = datetime.datetime.now()
 time_5days = time_now - datetime.timedelta(days=5)
+
+#datetime.timedelta(days=5) → Represents a time difference of 5 days.
 
 print("5 days ago date:", time_5days)
