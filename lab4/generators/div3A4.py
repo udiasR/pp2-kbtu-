592b8cdd87781705c82dbed7c3363da7d0cce434
udias <ulykbekdias6@gmail.com>
2025-03-15 15:01:42 +0500

    Update div3A4.py

--- lab4/generators/div3A4.py
+++ lab4/generators/div3A4.py
@@ -1,7 +1,10 @@
 def divisible_by3_and4(n):
     for i in range(0, n + 1):
         if i % 3 == 0 and i % 4 == 0:
-            yield i
+            yield i    
+            # Only keeps one number in memory
+            #Instead of storing all values in memory, it yields one at a time, making it memory-efficient.
+            # we use this instead of RETURN
 
 
 print(*divisible_by3_and4(30), sep=", ")
