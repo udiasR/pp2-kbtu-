5b6627f7e0b9e67e18513bd96da4d501f1ad7577
udias <ulykbekdias6@gmail.com>
2025-03-15 16:10:37 +0500

    Update json.py

--- lab4/json.py
+++ lab4/json.py
@@ -8,12 +8,16 @@ print(f"{'DN':<50} {'Description':<20} {'Speed':<10} {'MTU':<6}")
 print("-------------------------------------------------- -------------------  -------  -----")
 
 data = object.get("imdata", [])
+#If "imdata" is missing, it returns an empty list ([]) instead of causing an error.
 
-for i in data:
+for i in data:     #Each i is a dictionary 
     l1_Phys_If = i.get('l1PhysIf', {}).get('attributes', {})
     dn = l1_Phys_If.get('dn', '')
     desc = l1_Phys_If.get('descr', '')
-    speed = l1_Phys_If.get('speed', '')
-    mtu = l1_Phys_If.get('mtu', '')
+    speed = l1_Phys_If.get('speed', '')     #(inherit, auto, fixed)
+    mtu = l1_Phys_If.get('mtu', '')         #(Maximum Transmission Unit)
 
     print(f"{dn:<50}{desc:<22}{speed:<10}{mtu:<6}")
+
+
+# .get = Ensures missing values don’t break the program.
